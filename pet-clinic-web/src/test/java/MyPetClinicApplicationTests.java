17e675d49b23a9e288fe1d5b137b844d5a400d46
Mohammad Alidadi <alidadi.smtp@gmail.com>
2021-02-17 19:16:56 -0500

    packaging issue fixed

--- pet-clinic-web/src/test/java/MyPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/MyPetClinicApplicationTests.java
@@ -1,7 +1,8 @@
+import guru.springframework.mypetclinic.MyPetClinicApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = {MyPetClinicApplication.class})
 class MyPetClinicApplicationTests {
 
     @Test
