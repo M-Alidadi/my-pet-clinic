8bfe9a5f2615b3f8d79798d06b36ddd477f6b701
Mohammad Alidadi <alidadi.smtp@gmail.com>
2021-02-26 10:32:29 -0500

    Adding OwnerServiceMap.java

---
+++ pet-clinic-data/src/main/java/guru/springframework/mypetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,35 @@
+package guru.springframework.mypetclinic.services.map;
+
+import guru.springframework.mypetclinic.model.Owner;
+import guru.springframework.mypetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        this.deleteById(aLong);
+    }
+}
